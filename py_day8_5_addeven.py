0a80a6338356a850449d90a4b483c2c623a82889
Rprathap2000 <121160169+Rprathap2000@users.noreply.github.com>
2022-12-29 15:10:57 +0530

    Add files via upload

---
+++ py_day8_5_addeven.py
@@ -0,0 +1,19 @@
+##num1=int(input("Enter a no:"))
+##if num1%2==0:
+##    print(num1, 'is even')
+##if num1%2==1:
+##    print(num1, 'is odd')
+##print("we learnt if keyword")
+
+def checkeven():
+    num1=int(input("Enter a no:"))
+    if num1%2==0:
+        print(num1, 'is even')
+    return None
+def checkodd():
+    num1=int(input("Enter a no:"))
+    if num1%2==1:
+        print(num1, 'is odd')
+    return None
+checkeven()
+checkodd()
