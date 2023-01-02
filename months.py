c26533dfff9a7c6ab04b257840cfdec7635ad7e6
Rprathap2000 <121160169+Rprathap2000@users.noreply.github.com>
2023-01-02 16:56:32 +0530

    Add files via upload

---
+++ months.py
@@ -0,0 +1,36 @@
+##def displaymonths(num):
+##    n=" "
+##    if num==1:
+##        print("january")
+##    elif (num==2):
+##        print("February")
+##    elif (num==3):
+##        print("March")
+##    elif (num==4):
+##        print("April")
+##    elif (num==5):
+##        print("May")
+##    elif (num==6):
+##        print("jun")
+##    elif (num==7):
+##        print("july")
+##    elif (num==8):
+##        print("Augest")
+##    elif (num==9):
+##        print("Septembar")
+##    elif (num==10):
+##        print("October")
+##    elif (num==11):
+##        print("November")
+##    elif (num==12):
+##        print("December")
+##    else:
+##        print("Give the valid number")
+##n=int(input("How maney times u wnt to execute:"))
+##for i in range(n):
+##    intp=int(input("Enter the number"))
+##    displaymonths(intp)
+##
+
+
+
